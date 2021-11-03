1e6e10be132bd7df296c70563b09af64e0adeed5
Nikita Merzlyakov <merzlyakov02@gmail.com>
2021-11-04 00:01:04 +0300

    recognizer fix

--- recognizer/recognizer.py
+++ recognizer/recognizer.py
@@ -8,14 +8,18 @@ from song import Song
 class Recognizer:
     @staticmethod
     def recognize_API(music_bytes: bytes):
-        r = requests.post("https://nameless-sierra-10297.herokuapp.com/api/v1", files={"b_data": music_bytes})
-        song_data = json.loads(r.text)["data"]
-        song = Song(song_data[1]["track"]["title"],
-                    song_data[1]["track"]["subtitle"],
-                    f"{song_data[1]['track']['title']} - {song_data[1]['track']['subtitle']}",
-                    song_data[1]["track"]["share"].get("image"),
-                    song_data[1]["track"]["url"])
-        return song
+        try:
+            r = requests.post("https://nameless-sierra-10297.herokuapp.com/api/v1", files={"b_data": music_bytes})
+            song_data = json.loads(r.text)["data"]
+            song = Song(song_data[1]["track"]["title"],
+                        song_data[1]["track"]["subtitle"],
+                        f"{song_data[1]['track']['title']} - {song_data[1]['track']['subtitle']}",
+                        song_data[1]["track"]["share"].get("image"),
+                        song_data[1]["track"]["url"])
+            return song
+        except Exception as e:
+            logging.error(f"recognizer error {e}")
+            return None
 
 
     """Old version of recognize method"""
